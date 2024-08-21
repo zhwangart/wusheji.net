c4cfae05fb79d06a9c6f3fc75917fc03d96f43cc
zhwangart <zhwangart@163.com>
2024-08-21 14:27:01 +0800

    change colors

--- explore/bsccedu/js/resize.js
+++
@@ -1,7 +0,0 @@
-function handleResize() {
-	var htmlWidth = document.documentElement.clientWidth|| document.body.clientWidth
-	let htmlDom=document.getElementsByTagName('html')[0]
-	htmlDom.style.fontSize= 10 / 1920 * htmlWidth + 'px';
-}
-handleResize();
-window.onresize = handleResize;
